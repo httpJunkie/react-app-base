4e7ec7286ae347e55b55b77743038791cc0eb28a
Eric Bishard <e.bishard@gmail.com>
2020-05-21 07:27:20 -0700

    ack to starting app page

--- app/App.js
+++ app/App.js
@@ -1,31 +1,17 @@
 import React from 'react'
-import { RecoilRoot, atom } from 'recoil'
 
 import 'normalize.css'
 import '@progress/kendo-theme-material/dist/all.css'
 import './App.scss'
 
-//import { AppProvider } from "./context/AppContext"
+import { AppProvider } from "./context/AppContext"
 import Frame from './Frame'
-
-const themeDefault = 
-  (localStorage.getItem('spaceXLaunchApp_theme') || preferredTheme)
-
-const navOpenRecoil = atom({
-  key: navOpenRecoil,
-  default: false
-})
-
-const themeRecoil = atom({
-  key: themeRecoil,
-  default: false
-})
 
 const App = () => {
   return(
-    <RecoilRoot>
+    <AppProvider>
       <Frame />
-    </RecoilRoot>
+    </AppProvider>
   )
 }
 
